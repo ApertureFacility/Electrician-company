779e12231e924a24d8514a76b9bf70ae0d846b36
Sergey FrontendDeveloper <sergeyt.devwizard@gmail.com>
2025-10-05 19:12:50 +0300

    Create layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Layout from "./components/Layout";
 import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
-    <>
-      <Home/>
-    </>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Layout>
   );
 }
 
---
+++ src/components/Layout.tsx
@@ -0,0 +1,22 @@
+// components/Layout.tsx
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="app">
+      <Header />
+      <main className="main-content">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
